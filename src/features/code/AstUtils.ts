08e7e5c490d5ae76f7867cfd9c6b783402ef684a
cut0 <zeron9421@gmail.com>
2022-09-19 06:46:31 +0900

    :sparkles: AST の簡略化

--- src/features/code/AstUtils.ts
+++ src/features/code/AstUtils.ts
@@ -1,3 +1,5 @@
+import type { Node, NodePath } from "babel-traverse";
+
 export const convertAstString = async (
   code: string,
 ): Promise<string | undefined> => {
@@ -6,6 +8,61 @@ export const convertAstString = async (
     const ast = babylon.parse(code);
     const astString = JSON.stringify(ast, null, 2);
     return astString;
+  } catch {
+    console.info("パース失敗");
+    return undefined;
+  }
+};
+
+type CustomNode = {
+  id: number;
+  parentId?: number;
+  type: string;
+  postition: { start: number; end: number };
+  specificValue?: string;
+};
+
+export const convertCustomTree = async (code: string) => {
+  const babylon = await import("babylon");
+  const traverse = (await import("babel-traverse")).default;
+
+  const nodeList: CustomNode[] = [];
+
+  try {
+    const ast = babylon.parse(code);
+    traverse(ast, {
+      enter(
+        nodePath: NodePath & {
+          node: Node & {
+            name?: string;
+            value?: unknown;
+            operator?: string;
+          };
+        },
+      ) {
+        const baseNode = nodePath.node;
+        const parentId = nodeList.find((node) => {
+          return (
+            nodePath.parent.start === node.postition.start &&
+            nodePath.parent.end === node.postition.end
+          );
+        })?.id;
+
+        const nodeElement: CustomNode = {
+          id: nodeList.length,
+          parentId,
+          type: baseNode.type,
+          postition: { start: baseNode.start, end: baseNode.end },
+          specificValue:
+            baseNode.value !== undefined
+              ? String(baseNode.value)
+              : baseNode.name ?? baseNode.operator ?? undefined,
+        };
+        nodeList.push(nodeElement);
+      },
+    });
+
+    return nodeList;
   } catch {
     console.info("パース失敗");
     return undefined;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+      ],
+    },
+  },
 });
