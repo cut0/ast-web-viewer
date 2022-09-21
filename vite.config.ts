8e7946809a954dd75974c68305d919d9375527ef
cut0 <zeron9421@gmail.com>
2022-09-21 14:51:19 +0900

    :recycle: ポリフィル修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import { visualizer } from "rollup-plugin-visualizer";
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,7 +23,13 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      plugins: [visualizer()],
+      plugins: [visualizer(), rollupNodePolyFill()],
+    },
+  },
+  resolve: {
+    alias: {
+      buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
+      process: "rollup-plugin-node-polyfills/polyfills/process-es6",
     },
   },
 });
