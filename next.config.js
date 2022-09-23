edaa832a71135e5843a7bdd352aa938c887a63cd
cut0 <zeron9421@gmail.com>
2022-09-24 07:46:39 +0900

    :sparkles: bundle analyze 追加

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
 const withVanillaExtract = createVanillaExtractPlugin();
 
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = withVanillaExtract(nextConfig);
+module.exports = withBundleAnalyzer(withVanillaExtract(nextConfig));
